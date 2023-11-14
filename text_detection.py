a558de6fb71469550f3a622cdb77dd42d86ea5f6
hungtrieu07 <hungtrieu0701@gmail.com>
2023-11-14 14:00:57 +0700

    14112023

--- text_detection.py
+++ text_detection.py
@@ -1,32 +1,52 @@
-from paddleocr import PaddleOCR
-import cv2
-import os
 import glob
+import os
+
+import cv2
+from paddleocr import PaddleOCR
+from tqdm import tqdm
 
 # Initialize PaddleOCR
-ocr = PaddleOCR(lang='ch')
+ocr = PaddleOCR()
 
-for image_path in glob.glob("datasets/original_image/processed_image/*.jpg"):
-    image_name = os.path.basename(image_path)
-    # Load the image
-    image = cv2.imread(image_path)
+image_files = glob.glob("/home/hungtrieu07/Downloads/datasets/vietnamese_ocr/extract_images/*.jpg")
 
-    # Perform text detection
-    result = ocr.ocr(image, det=True)
+progress_bar = tqdm(total=len(image_files), desc='Processing images', unit='file')
 
-    # Create a directory to save cropped text images
-    output_dir = 'cropped_text_images'
-    os.makedirs(output_dir, exist_ok=True)
+for image_path in image_files:
+    try:
+        image_name = os.path.basename(image_path)
+        img = cv2.imread(image_path)
 
-    # Process the detected text regions
-    for detection_result in result[0][1]:
-        # Get the coordinates of the detected text box
-        coordinates = detection_result[0]
+        # Perform text detection
+        result = ocr.ocr(img, det=True, rec=False, cls=False)
+        result = result[:][:][0]
 
-        # Crop the detected text region from the original image
-        x1, y1, x2, y2 = coordinates
-        cropped_text = image[y1:y2, x1:x2]
+        # Create a directory to save cropped text images
+        output_dir = 'cropped_text_images_2'
+        os.makedirs(output_dir, exist_ok=True)
 
-        # Save the cropped text as individual images
-        output_path = os.path.join(output_dir, f'cropped_text_{coordinates}.png')
-        cv2.imwrite(output_path, cropped_text)
+        # Create Boxes
+        boxes = []
+        for line in result:
+            boxes.append([[int(line[0][0]), int(line[0][1])], [int(line[2][0]), int(line[2][1])]])
+
+        boxes = boxes[::-1]
+
+        EXPEND = 5
+        for box in boxes:
+            box[0][0] = box[0][0] - EXPEND
+            box[0][1] = box[0][1] - EXPEND
+            box[1][0] = box[1][0] + EXPEND
+            box[1][1] = box[1][1] + EXPEND
+
+        texts = []
+        for box in boxes:
+            cropped_image = img[box[0][1]:box[1][1], box[0][0]:box[1][0]]
+            output_path = os.path.join(output_dir, f'cropped_text_{box[0][0]}_{box[0][1]}_{box[1][0]}_{box[1][1]}.png')
+            cv2.imwrite(output_path, cropped_image)
+            progress_bar.update(1)
+    except Exception as e:
+        print(str(e))
+        pass
+
+progress_bar.close()
